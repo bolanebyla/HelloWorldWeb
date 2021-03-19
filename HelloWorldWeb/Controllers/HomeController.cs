978589a5f1aa90a3a7a7d2bac3860a2b8616d754
Алексей <alexleskov2000@gmail.com>
2021-03-19 11:00:33 +0800

    Маршрутизация и контроллер

---
+++ HelloWorldWeb/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public string Index(string id)
+        {
+            return "ID Value = " + id;
+        }
+    }
+}
--- HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/Startup.cs
@@ -24,6 +24,7 @@ namespace HelloWorldWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +51,9 @@ namespace HelloWorldWeb
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
