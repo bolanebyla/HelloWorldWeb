43ac63c60c2f4bd39b8d6376c68919f9f37a9f74
Алексей <alexleskov2000@gmail.com>
2021-03-19 11:13:47 +0800

    Кастомные контроллеры

---
+++ HelloWorldWeb/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldWeb.Controllers
+{
+    [Route("my/test/")]
+    public class TestController : Controller
+    {
+        [Route("show")]
+        public string Index()
+        {
+            return "Test Index method";
+        }
+        
+        [Route("details/{id}")]
+        public string Details(string id)
+        {
+            return "ID Value = " + id;
+        }
+    }
+}
